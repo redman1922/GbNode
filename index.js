4c026fe7c9c1ee76623e9dda71553b1dc2930a19
Denis_Davidov <redman1922svap@mail.ru>
2022-12-20 16:04:11 +0300

    add-timer
    lesson-2

--- index.js
+++ index.js
@@ -1,55 +1,87 @@
-function inter(arg) {
-    for (let k of arg) {
-        let z = +k;
-        if (Number.isInteger(z)) {
-        } else {
-            return false;
-        }
-    }
-    return true;
-}
+// import colors from "colors";
+import EventEmitter from "events";
 
-const colors = require('colors');
-const args = process.argv.slice(2);
-let flag = true;
+//Задание № 1
 
-if (inter(args) === true) {
-    if (+args[0] <= +args[1]) {
+// console.log(colors.red("Hello World!"));
+//
+// console.log("Record 1");
+// setTimeout(() => {
+//     console.log("Record 2");
+//     Promise.resolve().then(() => {
+//         setTimeout(() => {
+//             console.log("Record 3");
+//             Promise.resolve().then(() => {
+//                 console.log("Record 4");
+//             });
+//         });
+//     });
+// });
+// console.log("Record 5");
+// Promise.resolve().then(() =>
+//     Promise.resolve().then(() => console.log("Record 6"))
+// );
 
-        let n = +args[1];
-        let u = 0;
-        nextPrime:
-            for (let i = +args[0]; i <= n; i++) { // Для всех i...
+//Задание № 2
 
-                for (let j = 2; j < i; j++) { // проверить, делится ли число..
-                    if (i % j === 0) continue nextPrime; // не подходит, берём следующее
-                }
-                // светофор
-                if (u === 0) {
-                    console.log(colors.green(i));
-                    flag = false;
-                    u++;
-                } else if (u === 1) {
-                    console.log(colors.yellow(i));
-                    flag = false;
-                    u++;
-                } else if (u === 2) {
-                    console.log(colors.red(i))
-                    flag = false;
-                    u = 0;
-                }
+class TimerEmitter extends EventEmitter {}
+const emitter = new TimerEmitter(); //создание emitter
 
-            }
+//регистрация события отсчета времени
+emitter.on("timerTick", ([dateInFuture, timer]) => {
+    const dateNow = new Date(); //создание даты на момент старта для сравнения с dateInFuture
+    if (dateNow >= dateInFuture) {
+        emitter.emit("timerEnd", timer); //генерация события timerEnd
     } else {
-        console.log('Первое больше второго');
-        flag = false;
+        console.log(getPrettyTime((dateInFuture - dateNow) / 1000), " left"); //вызов функции для вычисления оставшегося времени
     }
+});
 
-} else {
-    console.log('Не число');
-    flag = false;
+//регистрация события окончания времени
+emitter.on("timerEnd", timer => {
+    console.log("Time is Up!");
+    clearInterval(timer);
+});
+
+//функция вычисления дней, часов, минут, секунд
+const getPrettyTime = seconds => {
+    const arr = [
+        Math.floor(seconds % 60),
+        Math.floor((seconds / 60) % 60),
+        Math.floor(seconds / (60 * 60)) % 24,
+        Math.floor(seconds / (60 * 60 * 24)),
+    ];
+    return `${arr.pop()} days ${arr.pop()} hours ${arr.pop()} minutes ${arr.pop()} seconds`;
+};
+
+//создание кнопки старт
+const start = dateInFuture => {
+    setInterval(function () {
+        //генерация события timerTick, передача данных dateInFuture
+        emitter.emit("timerTick", [dateInFuture, this]); //this = timer
+    }, 1000);
+};
+
+//цикл для ввода нескольких аргументов при запуске программы
+for (const arg of process.argv.slice(2)) {
+    const [hour, day, month, year] = arg.split("-");
+    const dateInFuture = new Date(Date.UTC(year, month - 1, day, hour));
+
+    if (isNaN(dateInFuture)) {
+        emitter.emit("error", new Error("Что-то пошло не так!"));
+    }
+    start(dateInFuture);
 }
 
-if (flag == true) {
-    console.log('Простых чисел в диапазоне нет'.red);
-}
+// //передача аргументов при вызове
+// const [hour, day, month, year] = process.argv[2].split("-");
+
+// //создание времени отсчета
+// const dateInFuture = new Date(Date.UTC(year, month - 1, day, hour));
+
+// if (isNaN(dateInFuture)) {
+//   //обработка ошибки ОБЯЗАТЕЛЬНА!!!
+//   emitter.emit("error", new Error("Что-то пошло не так!"));
+// }
+// // запуск программы
+// start(dateInFuture);
