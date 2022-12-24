a12513865eece75f1d6b153e32a56128c1d145d0
Denis_Davidov <redman1922svap@mail.ru>
2022-12-24 16:42:36 +0300

    lesson-5
    lesson-5

--- index.js
+++ index.js
@@ -1,74 +1,63 @@
-import colors from "colors";
-import readline from "readline";
+import http from "http";
+import fs from "fs";
 import path from "path";
-import inquirer from "inquirer";
-import fsp from "fs/promises";
+import { Transform } from "stream";
 
-console.log(colors.red("Hello World!"));
+const host = "localhost";
+const port = 3000;
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+const list = [];
+const fsp = fs.promises;
 
-const root = process.cwd();
-
-const findFilesInDir = dirName => {
-    //читаем список файлов и директорий
-    return fsp
-        .readdir(dirName)
-        .then(choices => {
-            return inquirer.prompt([
-                { name: "fileName", type: "list", message: "Choose file", choices },
-                {
-                    name: "findString",
-                    type: "input",
-                    message: "Enter something for search",
-                    //асинхронная функция проверки на файл или директорию
-                    async when({ fileName }) {
-                        const fullPath = path.join(dirName, fileName);
-                        const stat = await fsp.stat(fullPath);
-
-                        return stat.isFile();
-                    },
-                },
-            ]);
-        })
-        .then(async ({ fileName, findString }) => {
-            const fullPath = path.join(dirName, fileName);
-            if (findString === undefined) return findFilesInDir(fullPath);
-
-            return Promise.all([
-                fsp.readFile(fullPath, "utf-8"),
-                Promise.resolve(findString),
-            ]);
-        })
-        .then(result => {
-            if (result) {
-                const [text, findString] = result;
-                const pattern = new RegExp(findString, "g");
-                let count = 0; // для подсчета совпадений
-
-                //вывод текста заменяем строки на выделенные красным цветом
-                const out = text.replace(pattern, () => {
-                    count++;
-                    //помечаем красным цветом совпадения
-                    return colors.red(findString);
-                });
-
-                console.log(out, "\n", colors.green(`Found ${count} values`));
-            }
-        });
+const links = (arr, curUrl) => {
+  if (curUrl.endsWith("/")) curUrl = curUrl.substring(0, curUrl.length - 1);
+  let li = "";
+  for (const item of arr) {
+    li += `<li><a href="${curUrl}/${item}">${item}</a></li>`;
+  }
+  return li;
 };
 
-//точка входа
-rl.question(
-    `You are in ${root} \n Please enter the path to the directory: `,
-    dirPath => {
-        const dirName = path.join(root, dirPath);
+const server = http.createServer((req, res) => {
+  if (req.method === "GET") {
+    const url = req.url.split("?")[0];
+    const curPath = path.join(process.cwd(), url);
 
-        findFilesInDir(dirName);
-    }
+    fs.stat(curPath, (err, stats) => {
+      if (!err) {
+        if (stats.isFile(curPath)) {
+          const rs = fs.createReadStream(curPath, "utf-8");
+          rs.pipe(res);
+        } else {
+          fsp
+            .readdir(curPath)
+            .then((files) => {
+              if (url !== "/") files.unshift("..");
+              return files;
+            })
+            .then((data) => {
+              // render
+              const filePath = path.join(process.cwd(), "./index.html");
+              const rs = fs.createReadStream(filePath);
+              const ts = new Transform({
+                transform(chunk, encoding, callback) {
+                  const li = links(data, url);
+                  this.push(chunk.toString().replace("#filelinks#", li));
+
+                  callback();
+                },
+              });
+
+              rs.pipe(ts).pipe(res);
+            });
+        }
+      } else {
+        res.end("Path not exists");
+      }
+    });
+  }
+});
+
+server.listen(port, host, () =>
+  console.log(`Server running at http://${host}:${port}`)
 );
-
-rl.on("close", () => process.exit(0));
